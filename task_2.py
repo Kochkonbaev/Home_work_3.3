f095678aa85675c6cf6712cf98a6b9cfa1290861
Abdimusa <musa301196@gmail.com>
2021-06-24 14:57:35 +0300

    Задание 2

---
+++ task_2.py
@@ -0,0 +1,25 @@
+class Pizza:
+
+    def __init__(self, name, ingredients):
+        self.name = name
+        self.ingredients = ingredients
+    
+    @classmethod
+    def margarita(sost):
+        name = 'Margarita'
+        ingredients = ['mozarella', 'tomatos', 'olives'] 
+        return sost(name, ingredients)
+
+    @staticmethod
+    def calculate_area(radius):
+        pi = 3.14
+        return pi * radius ** 2
+    
+
+    
+    
+
+marg = Pizza.margarita()
+print(marg.name)
+print(marg.ingredients)
+print(marg.calculate_area(radius=50))
